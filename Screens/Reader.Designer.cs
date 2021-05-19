819f52bbec1abcef934714bd94e03e4212e12242
Timothy Ganoza <jostimian@gmail.com>
2021-05-19 22:17:05 +0800

    [➕ Feature Added] Icon Added To Reader

--- Screens/Reader.Designer.cs
+++ Screens/Reader.Designer.cs
@@ -29,9 +29,10 @@ namespace Laps_Remote.Screens
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Reader));
 			this.MonitorAReader = new System.Windows.Forms.DataVisualization.Charting.Chart();
 			this.VitalSelectorAReader = new System.Windows.Forms.ComboBox();
 			((System.ComponentModel.ISupportInitialize)(this.MonitorAReader)).BeginInit();
@@ -44,20 +45,20 @@ namespace Laps_Remote.Screens
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.MonitorAReader.BackColor = System.Drawing.SystemColors.Control;
 			this.MonitorAReader.BorderlineColor = System.Drawing.Color.Transparent;
-			chartArea1.Name = "vitalArea";
-			this.MonitorAReader.ChartAreas.Add(chartArea1);
-			legend1.BackColor = System.Drawing.Color.White;
-			legend1.Enabled = false;
-			legend1.Name = "Legend1";
-			this.MonitorAReader.Legends.Add(legend1);
+			chartArea2.Name = "vitalArea";
+			this.MonitorAReader.ChartAreas.Add(chartArea2);
+			legend2.BackColor = System.Drawing.Color.White;
+			legend2.Enabled = false;
+			legend2.Name = "Legend1";
+			this.MonitorAReader.Legends.Add(legend2);
 			this.MonitorAReader.Location = new System.Drawing.Point(-3, 45);
 			this.MonitorAReader.Name = "MonitorAReader";
 			this.MonitorAReader.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
-			series1.ChartArea = "vitalArea";
-			series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
-			series1.Legend = "Legend1";
-			series1.Name = "Vital";
-			this.MonitorAReader.Series.Add(series1);
+			series2.ChartArea = "vitalArea";
+			series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+			series2.Legend = "Legend1";
+			series2.Name = "Vital";
+			this.MonitorAReader.Series.Add(series2);
 			this.MonitorAReader.Size = new System.Drawing.Size(776, 393);
 			this.MonitorAReader.TabIndex = 4;
 			this.MonitorAReader.Text = "vital";
@@ -84,6 +85,7 @@ namespace Laps_Remote.Screens
 			this.ClientSize = new System.Drawing.Size(800, 450);
 			this.Controls.Add(this.VitalSelectorAReader);
 			this.Controls.Add(this.MonitorAReader);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "Reader";
 			this.Text = "Reader";
 			this.Load += new System.EventHandler(this.Reader_Load);
